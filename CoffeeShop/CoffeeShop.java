cf72e4618ea70d7c37a8daa14b5914c885cbf027
Caitlyn Bell <caitlyn_bell@m013498.aw.org>
2017-09-15 13:19:56 -0700

    Updated CoffeeShop.java

--- CoffeeShop/CoffeeShop.java
+++ CoffeeShop/CoffeeShop.java
@@ -4,10 +4,10 @@ public class CoffeeShop{ // create the Coffee Shop class
 public static void main(String[]args){ //creates the main method
 System.out.println("Hello Welcome to Rocky's Coffee Shop! May I take your order?");// prints the line
 Scanner scan = new Scanner (System.in); // creaters new scanner
-String customersOrder = scan.next(); // creates object that is scanned
+String customersOrder = scan.nextLine(); // creates object that is scanned
 System.out.println("What is your name?");
-String customerName = scan.next(); // creates the name string
+String customerName = scan.nextLine(); // creates the name string
 System.out.println ("Ok, "+ customerName +", your "+ customersOrder + " will be ready in a bit."); // prints cutomers name and order
-System.out.println(customerName + "! Your "+ customersOrder + " is ready!"); // prints name and order is ready
+System.out.println(customerName +"! Your "+ customersOrder + " is ready!"); // prints name and order is ready
 }
 }
