b4e949bd489c7b141d62c15d02dd75053f9c22ae
Caitlyn Bell <caitlyn_bell@M013498.local>
2017-10-09 18:59:05 -0700

    updated refresher

--- Refresher.java
+++ Refresher.java
@@ -1,18 +1,110 @@
 //Anna and Caitlyn September 27, 2017
 import java.util.Scanner; //imports the scanner class
-import java.io.*; // improts the txt file reader class, I used https://www.caveofprogramming.com/java/java-file-reading-and-writing-files-in-java.html as a resource
+import java.io.*; // improts the txt file reader class, I used http://javaconceptoftheday.com/find-most-repeated-word-in-text-file-in-java/ as a resource
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Set;
+
 public class Refresher{ // create the Refresher class
 public static void main(String[]args){ //creates the main method
-String topicFile = "topics.txt"; //this is the name of the file as an object
-try {
-FileReader fileFun = new FileReader(topicFile); // reads the inputted file in encoding
-BufferedReader bufferedFun = new BufferedReader(fileFun); //wraps the inputed file to be read
-//while ((line = bufferedFun.readLine()) != null)
-bufferedFun.close(); //closing the file
+
+  //Creating wordCountMap which holds words as keys and their occurrences as values
+
+HashMap<String, Integer> wordCountMap = new HashMap<String, Integer>();
+ BufferedReader reader = null;
+try
+       {
+  //Creating BufferedReader object
+reader = new BufferedReader(new FileReader("C:sample.txt"));
+            //Reading the first line into currentLine
+            String currentLine = reader.readLine();
+
+           while (currentLine != null)
+           {
+               //splitting the currentLine into words
+
+               String[] words = currentLine.toLowerCase().split(" ");
+
+               //going through each word
+
+               for (String word : words)
+               {
+                   //if word is already present in wordCountMap, updating its count
+
+                   if(wordCountMap.containsKey(word))
+                   {
+                       wordCountMap.put(word, wordCountMap.get(word)+1);
+                   }
+
+                   //otherwise inserting the word as key and 1 as its value
+                   else
+                   {
+                       wordCountMap.put(word, 1);
+                   }
+               }
+
+               //Reading next line into currentLine
+
+               currentLine = reader.readLine();
+           }
+
+           //Getting the low risk
+
+           String lowRisk = null;
+
+           int count = 0;
+
+           Set<Entry<String, Integer>> entrySet = wordCountMap.entrySet();
+
+           for (Entry<String, Integer> entry : entrySet)
+           {
+               if(entry.getValue() > count)
+               {
+                   lowRisk = entry.getKey();
+                   count = entry.getValue();
+               }
+           }
+           // Getting the high risk
+           String highRisk = null;
+ for (Entry<String, Integer> entry : entrySet)
+           {
+               if(entry.getValue() < count)
+               {
+                   highRisk = entry.getKey();
+                   count = entry.getValue();
+               }
+           }
+
+
+           System.out.println("Hey pal! What would you like to discuss today?(choose: low risk, high risk, or new-topic)");
+           Scanner scan = new Scanner (System.in); // creaters new scanner
+           String riskLevel = scan.nextLine(); // creates object that is scanned
+           if(riskLevel == "low risk"){
+             System.out.println("Have you heard anything new about " + lowRisk + "?");
+           }
+           else if (riskLevel == "high risk"){
+            System.out.println("How are you feeling about " + highRisk + "?");
+           }
+
+       }
+       catch (IOException e)
+       {
+           e.printStackTrace();
+       }
+       finally
+       {
+           try
+           {
+               reader.close();           //Closing the reader
+           }
+           catch (IOException e)
+           {
+               e.printStackTrace();
+           }
+       }
+
 }
-catch(FileNotFoundException ex)
-
-
-
-
-System.out.println("Hey pal! What would you like to discuss today?(choose: low risk, high risk, or new-topic)");
+}
