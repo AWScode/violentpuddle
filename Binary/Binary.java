66eb5e69adaa3ea2787c35109c02e0e4305e1f3e
Caitlyn Bell <caitlyn_bell@m013498.aw.org>
2017-12-06 13:52:39 -0800

    First draft

---
+++ Binary/Binary.java
@@ -0,0 +1,29 @@
+//Caitlyn Bell's binary 12/6/2017
+import java.util.*; //import the utility class
+import java.util.Scanner; //imports the scanner class
+public class Binary{
+  public static void main(String [] args){
+
+    Int binOne[] = new int[8]; //Make an array of int for binary 1
+    Int binTwo[] = new int[8]; //Make an array of int for binary 2
+
+    System.out.println("Please input first binary number:"); //Scanner to get first binary number
+    Scanner binOne[] = new Scanner(System.in);
+
+    System.out.println("Please input second binary number:");//Scanner to get second binary number
+    Scanner binTwo[] = new Scanner(System.in); 
+
+    System.out.println("Choose (add, subtract, multiply, divide)"); //Scanner to get the arithmetic function wanted
+    Scanner atith = new Scanner(System.in);
+
+
+public static int addBinary(){}// method to add the binaries
+
+public static int subtractBinary(){} // method to subtract the binaries
+
+public static int multiplyBinary(){} // method to multiply the binaries
+
+public static int divideBinary(){}   // method to divide the binaries
+
+  }
+}
