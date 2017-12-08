9e9abde2d2b98b6ec1d459f633bdbd8b197282fd
Caitlyn Bell <caitlyn_bell@m013498.aw.org>
2017-12-08 13:47:08 -0800

    new 2

--- Binary/Binary.java
+++ Binary/Binary.java
@@ -2,28 +2,56 @@
 import java.util.*; //import the utility class
 import java.util.Scanner; //imports the scanner class
 public class Binary{
+  int i = 0;
+  int remainder =0;
+  int binOne = new int[8]; //Make an array of int for binary 1
+  int binTwo = new int[8]; //Make an array of int for binary 2
+  int binNew = new int[8]; // makes the new array that final binary
+  public static int addBinary(){
+    if(binOne[0].equals(0) && binTwo[0].equals(0)){
+    binNew[0]= 0;
+    else if (binOne[0].equals(1) && binTwo[0].equals(1))
+    binNew[0]= 0;
+    remainder = 1;
+    else binNew = 1;}
+    if (binOne[1].equals(0) && binTwo[1].equals(0)){
+    binNew[1]= 0;
+    else if (binOne[1].equals(1) && binTwo[1].equals(1)&& remainder.equals(1))
+    binNew[0]= 0;
+    remainder = 0;
+    else binNew = 1;}
+
+  }// method to add the binaries
+
+  public static int subtractBinary(){} // method to subtract the binaries
+
+  public static int multiplyBinary(){} // method to multiply the binaries
+
+  public static int divideBinary(){}   // method to divide the binaries
+
   public static void main(String [] args){
 
-    Int binOne[] = new int[8]; //Make an array of int for binary 1
-    Int binTwo[] = new int[8]; //Make an array of int for binary 2
+
+    Scanner scan = new Scanner (System.in); // creaters new scanner
 
     System.out.println("Please input first binary number:"); //Scanner to get first binary number
-    Scanner binOne[] = new Scanner(System.in);
+    int binOne = scan.nextInt(); //scans for next int
 
     System.out.println("Please input second binary number:");//Scanner to get second binary number
-    Scanner binTwo[] = new Scanner(System.in); 
+    int binTwo = scan.nextInt(); //scans for next int
 
     System.out.println("Choose (add, subtract, multiply, divide)"); //Scanner to get the arithmetic function wanted
-    Scanner atith = new Scanner(System.in);
+    String atith = scan.nextLine();
+
+    if(atith.equals("add")){
+      System.out.println("Sum of the binary numbers: " + addBinary);
+
+      else if(atith.equals("subtract")){
+        System.out.println("")
+      }
+    }
 
 
-public static int addBinary(){}// method to add the binaries
-
-public static int subtractBinary(){} // method to subtract the binaries
-
-public static int multiplyBinary(){} // method to multiply the binaries
-
-public static int divideBinary(){}   // method to divide the binaries
 
   }
 }
