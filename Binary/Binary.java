bbc9e6131bdb9c56195ed6dc895cd56956dcb1dd
Caitlyn Bell <caitlyn_bell@M013498.local>
2017-12-12 20:04:31 -0800

    multiply

--- Binary/Binary.java
+++ Binary/Binary.java
@@ -6,10 +6,10 @@ public class Binary{
     int i = 0, rem = 0;
     Scanner scan = new Scanner (System.in); // creaters new scanner
     System.out.println("Please input first binary number:"); //Scanner to get first binary number
-    long binOne = scan.nextInt(); //scans for next int
+    long binOne = scan.nextLong(); //scans for next int
     System.out.println("Please input second binary number:");//Scanner to get second binary number
-    long binTwo = scan.nextInt(); //scans for next int
-  public static int addBinary(long binOne, long binTwo, int i, int rem){
+    long binTwo = scan.nextLong(); //scans for next int
+  public static long addBinary(long binOne, long binTwo, int i, int rem){
     int[] sum = new int[20];
     while (binOne !0 || binTwo != 0){
       sum[i++] = (int)((binOne % 10 + binTwo % 10 + rem)% 2); // takes the binary and puts it into decimal
@@ -22,12 +22,30 @@ public class Binary{
       --i;
   }// method to add the binaries
 
-  public static int subtractBinary(){} // method to subtract the binaries
+  public static long subtractBinary(){} // method to subtract the binaries
 
 
-  public static int multiplyBinary(){} // method to multiply the binaries
+  public static long multiplyBinary(long binOne, long binTwo){
+  long multiply = 0;
+  int digit, factor = 1;
+  while (binTwo != 0)
+  {
+   digit = (int)(binTwo % 10);
+   if (digit == 1)
+   {
+    binOne = binOne * factor;
+    multiply = addBinary((int) binOne, (int) multiply);
+   } 
+   else
+   {
+    binOne = binOne * factor;
+   }
+   binTwo = binTwo / 10;
+   factor = 10;
+  }
+ } // method to multiply the binaries
 
-  public static int divideBinary(){int binOne, int binTwo, int i} {
+  public static long divideBinary(){long binOne, long binTwo, int i} {
     int quo =0;
     while (binOne <= binTwo && binOne > 0){
           binOne <<=1;
