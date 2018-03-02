1dd5907f138945ebfc76fbe70c52f03a81fb27bf
Caitlyn Bell <caitlyn_bell@m013498.aw.org>
2018-03-02 13:32:19 -0800

    update

--- IA/cookbook.java
+++ IA/cookbook.java
@@ -3,10 +3,17 @@ import java.lang.*;
 
 public class cookbook{
 // how do I put all of these arrays into one array of recipes?
+  String[] master =  new String[4];
+
   public ArrayList<String> recipeName = new ArrayList<String>(); //what it is called
-  public ArrayList<Int> timePrep = new ArrayList<Int>(); //integer in minutes
+  public ArrayList<String> timePrep = new ArrayList<Int>(); //integer in minutes
   public ArrayList<String> ingredients = new ArrayList<String>(); //what you need
   public ArrayList<String> directions = new ArrayList<String>(); //what you need to do
+
+  master [0] = recipeName; // stores the RN into position 0
+  master [1] = timePrep;  //stores the TP into position 1
+  master [2] = ingredients; //stores the I into position 2
+  master [3] = directions; // stores the d into position 3
 
 public void setRecipeName(ArrayList<String> recipeName){
   this.recipeName = recipeName;
@@ -23,7 +30,7 @@ public void setDirections(ArrayList<String> directions){
 public String getRecipeName(int i){
   return recipeName.get(i);
 }
-public Int getTimePrep(int i){
+public String getTimePrep(int i){
   return timePrep.get(i);
 }
 public String getIngredients(int i){
@@ -40,24 +47,56 @@ public static void main(String[] args){
   if(cat.equals("Add new recipe")){
     System.out.println("Please enter the recipe name.");
     Scanner scan2 = new Scanner(System.in);
-    String recipeName = scan2.nextLine();
+    String recipeName[i] = scan2.nextLine(); // does saying [i] make it all the same position
 
     System.out.println("Please enter the time prep.");
     Scanner scan3 = new Scanner(System.in);
-    String timePrep = scan3.nextLine();
+    String timePrep[i] = scan3.nextLine();
 
     System.out.println("Please enter the ingredient list." );
     Scanner scan4 = new Scanner(System.in);
-    String ingredients = scan4.nextLine();
+    String ingredients[i] = scan4.nextLine();
 
     System.out.println("Please enter the directions.");
     Scanner scan5 = new Scanner(System.in);
-    String directions = scan5.nextLine();
+    String directions[i] = scan5.nextLine();
   }
   if(cat.equals("Find a recipe")){
-    System.out.println(" \n Would you like to: Enter \"search by recipe name\" or \"Search by time prep\" or \"Search by ingredients\" ? ");
+    System.out.println(" \n Would you like to: Enter \"search by recipe name\" or \"search by time prep\" or \"search by ingredients\" ? ");
     Scanner scanscan = new Scanner(System.in);
-    S
+    String dog = scan.nextLine(); // dog represents what they are searching by
+
+    if(dog.equals("search by recipe name")){
+      System.out.println("Please enter the name of the recipe");
+      Scanner scanscan1 = new Scanner(System.in);
+      String name = scan.nextLine();
+      // search master [0] for name
+      // find what row position name is in
+      // return the all columns with the row position found
+      return ("x"); // this is a filler to compile code FIX
+    }
+    if(dog.equals("search by time prep")){
+      System.out.println("Please enter the time prep");
+      Scanner scanscan2 = new Scanner(System.in);
+      String time = scan.nextLine();
+      // search master [1] for time
+      // find what row position time is in
+      // return all the columns with the row position found
+      return ("x"); // this is a filler to compile code FIX
+    }
+    if (dog.equals("search by ingredients")){
+      System.out.println("Please enter the ingredient");
+      Scanner scanscan3 = new Scanner(System.in);
+      String userIngredients = scan.nextLine();
+      //this would be cool if I could search for multiple ingredients but IDK how
+      // search master [2] for userIngredients
+      //find what row position userIngredients is in
+      //return all the columns with the row position found
+    return ("x"); // this is a filler to compile code FIX 
+    }
+    }
+return ("grilled cheese, 10 minutes, 1 piece of cheese and 2 pieces of bread, heat stove and place bread on stove until golden brown flip twice");
   }
+
 }
 }
