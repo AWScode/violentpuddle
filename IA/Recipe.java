73c00fef7e776dd05360bd6f8895b3e6c8b2a9b6
Caitlyn Bell <caitlyn_bell@M013498.local>
2018-03-08 22:45:03 -0800

    recipe class

---
+++ IA/Recipe.java
@@ -0,0 +1,39 @@
+// program to interact with hashmap to create the recipes within the cookbook
+
+public class Recipe{
+  private String name; // creates name object
+  private int timePrep; //creates the time it takes to cook
+  private String ingredients; //creates ingredients object
+  private String directions; //creates directions object
+
+public Recipe(String name, int timePrep, String ingredients, String directions){ //contructor
+  this.name = name;
+  this.timePrep = timePrep;
+  this.ingredients = ingredients;
+  this.directions = directions;
+}
+public void setName(String name){
+  this.recipeName = Name; //sets name
+}
+public void setTimePrep(Int timePrep){
+  this.timePrep = timePrep; //sets timePrep
+}
+public void setIngredients(String ingredients){
+  this.ingredients = ingredients; //sets ingredients
+}
+public void setDirections(String directions){
+  this.directions = directions; //sets directions
+}
+public String getName(){
+  return name;
+}
+public Int getTimePrep(){
+  return timePrep;
+}
+public String getIngredients(){
+  return ingredients;
+}
+public String getDirections(){
+  return directions;
+}
+}
