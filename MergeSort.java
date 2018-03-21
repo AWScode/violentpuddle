5eaf7276faa024f8d8f343a3094e399823f10a72
Caitlyn Bell <caitlyn_bell@M013498.local>
2018-03-20 22:29:02 -0700

    (first try

---
+++ MergeSort.java
@@ -0,0 +1,20 @@
+public class MergeSort{
+  ArrayList<String> words;
+
+  public MergeSort{
+    ArrayList<String> words = new ArrayList<String>();
+
+  public static String wLengthMergeSort(){
+      int max = 0;
+      int count = 0;
+      for int i= 0; i < words.size(); i++){
+        count ++;
+        words[i]= count;
+        if (words[i] > max){
+        max == words[i];
+        }
+        return max; 
+      }
+  }
+  }
+}
