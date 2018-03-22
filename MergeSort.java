f415a55962c1dcd4a53356caf66391c1569b83b0
Caitlyn Bell <caitlyn_bell@M013498.local>
2018-03-21 17:01:42 -0700

    new

--- MergeSort.java
+++ MergeSort.java
@@ -3,8 +3,41 @@ public class MergeSort{
 
   public MergeSort{
     ArrayList<String> words = new ArrayList<String>();
+    public ArrayList<String> getList(){
+      return words;
+    }
+  public ArrayList<String> merge(ArrayList<String> Sorted1, ArrayList<String> Sorted1) {
+    //combine the two
+    int index1 =0;
+    int index2 =0;
+    ArrayList<String> finalList = new ArrayList<String>();
+    for(int i =0; i<sorted1.size() + sorted2.size(); i++){
+      if (sorted1.get(index1) <= sorted2.get(index2)){
+        finalList.add(sorted1.get(index1));
+        index1++;
+      }
+      else{
+          finalList.add(sorted2.get(index2));
+          index2++
+        }
+      }
+    return finalList;
 
-  public static String wLengthMergeSort(){
+  }
+  public ArrayList<String> wLengthMergeSort(ArrayList<String> myList){
+      if(myList.size() <= 1){
+        return myList;
+      }
+      else {
+        ArrayList<String> list1= //first half of list
+        ArrayList<String> list2 = //second half of list
+
+        ArrayList<String> sorted1= wLengthMergeSort(list1);
+        ArrayList<String> sorted2 = wLengthMergeSort(list2);
+        ArrayList<String> sortedFinal = merge(sorted1,sorted2);
+        return sortedFinal;
+      }
+
       int max = 0;
       int count = 0;
       for int i= 0; i < words.size(); i++){
@@ -13,7 +46,7 @@ public class MergeSort{
         if (words[i] > max){
         max == words[i];
         }
-        return max; 
+        return max;
       }
   }
   }
